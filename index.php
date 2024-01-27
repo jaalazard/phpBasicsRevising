3816fd5c17ab0d0efe5302261dd64472b51fb2ac
jaalazard <ja.alazard@proton.me>
2024-01-27 13:53:31 +0100

    conditions

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-$hello = 'hello';
-$world = 'world';
+$weapons = ['fists', 'whip', 'gun'];
+$opponentWeapon = $weapons[rand(0, 2)];
+$indyWeapon = $weapons[rand(0, 2)];
 
-echo $hello . ' ' . $world;
+echo ($indyWeapon == $weapons[0] && $opponentWeapon == $weapons[1] || $indyWeapon == $weapons[1] && $opponentWeapon == $weapons[2] || $indyWeapon == $weapons[2] && $opponentWeapon == $weapons[0]) ? "Indy wins !" :
+(($opponentWeapon == $weapons[0] && $indyWeapon == $weapons[1] || $opponentWeapon == $weapons[1] && $indyWeapon == $weapons[2] || $opponentWeapon == $weapons[2] && $indyWeapon == $weapons[0]) ? "The opponent wins !" : "Draw !");

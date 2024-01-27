d4f1f93c5ba3a386ad5c170b78a94afc08c2a2f2
jaalazard <ja.alazard@proton.me>
2024-01-27 13:47:48 +0100

    indy

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
-$hello = 'hello';
-$world = 'world';
-
-echo $hello . ' ' . $world;
+$nomFilm = 'Indiana Jones et la dernière croisade';
+$viewed = false;
+$release = 2000;
+$rating = 1.5;
+$didIViewIt = '';
+if ($viewed) {
+    $didIViewIt = "oui";
+} else {
+    $didIViewIt = "non";
+};
+echo "le film s'appelle " . $nomFilm . " il est sorti en " . $release . " je l'ai vu ? => " . $didIViewIt ;

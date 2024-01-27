6dacb06dd9742b2cded47d4115ce61750d089a66
jaalazard <ja.alazard@proton.me>
2024-01-27 14:57:45 +0100

    trees

--- index.php
+++ index.php
@@ -1,28 +1,24 @@
 <?php
 
-/* Exercice 3 : Dangerous animals
-Please find some data about various animals dangerosity
-    🐻 600kg, carnivorous => dangerous
-    🐑 60kg, not carnivorous => not dangerous
-    🐘 1200kg, not carnivorous => dangerous
-    🦔 1kg, carnivorous => not dangerous
-    🐇 3kg, not carnivorous => not dangerous
-    🦖 8000kg, carnivorous => dangerous
-Create a condition to determine if an animal is dangerous or not. The rules are :
-    dangerous if the animal is carnivorous AND weights more than 50kg, OR if it weights more than 1000kg. Display 'Ouch, you should run !'
-    In all other case, display 'It is gentle as a lamb 🐑'
+/* Exercise 1 : planting trees 🌳
+    Write a loop to plant 3 trees (you should display '🌳🌳🌳').
+    Write a loop to plant 8 🌳 .
+    Write a loop to plant 20 🌳 .
  */
 
- function isItDangerous($kilos, $isCarnivorous) {
-    if ($kilos > 50 && $isCarnivorous || $kilos > 1000) {
-        $result = 'Ouch, you should run !';
-    } else {
-        $result = 'It is gentle as a lamb 🐑';
-    }
-    return $result;
+ for ($i = 0; $i < 3; $i++){
+    echo '🌳';
+ }
+ echo '   ';
+
+ for ($i = 0; $i < 8; $i++){
+    echo '🌳';
  }
 
-$isCarnivorous = false;
-$weight = 600;
+ echo '   ';
 
-echo isItDangerous($weight, $isCarnivorous);
+ for ($i = 0; $i < 20; $i++){
+    echo '🌳';
+ }
+
+ echo '   ';
